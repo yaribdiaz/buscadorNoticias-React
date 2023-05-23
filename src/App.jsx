46038e282363558fb0ebc45b1a1aa9b02f2b0eac
Yarib Díaz <yaribdiazz@gmail.com>
2023-05-22 19:20:38 -0600

    Inicio de App

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Container, Grid, Typography } from "@mui/material"
+import Formulario from "./components/Formulario"
+
+
+function App() {
+
+  return (
+    <>
+     <Container>
+        <header>
+          <Typography
+              align="center" marginY={5} component="h1" variant="h3" 
+          >
+              Buscador de Noticias
+          </Typography>
+        </header>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item md={6} lg={4}>
+            <Formulario/>
+          </Grid>
+        </Grid>
+     </Container>
+    </>
+  )
+}
+
+export default App
